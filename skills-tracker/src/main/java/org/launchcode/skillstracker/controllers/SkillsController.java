650239d781f993e09830e858aedf25bb388f9f94
krystalwhite <krystalsnowhite@gmail.com>
2021-09-06 19:51:43 -0500

    made a table

--- skills-tracker/src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ skills-tracker/src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -1,6 +1,7 @@
 package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -67,32 +68,39 @@ public class SkillsController {
     }
 
 
-//    @RequestMapping(value = "skills", method = {RequestMethod.GET, RequestMethod.POST})
-    @PostMapping("form")
+    @RequestMapping(value = "skills", method = {RequestMethod.GET, RequestMethod.POST})
+//    @PostMapping("form")
     @ResponseBody
     public String compileNameAndLanguages(@RequestParam String name, @RequestParam String firstLanguageSelect, @RequestParam String secondLanguageSelect, @RequestParam String thirdLanguageSelect) {
         String response =
-                "<html>" +
-                "<body>" +
-                "<p style='color:#0394fc'><font size='+15'>Name: " + name + "</font></p>" +
-                "<ol>" +
-                "<font size='+5'><li>" + firstLanguageSelect + "</li>" +
-                "<li>" + secondLanguageSelect + "</li>" +
-                "<li>" + thirdLanguageSelect + "</li></font>" +
-                "</ol>" +
-                "</body>" +
-                "</html>";
+//                "<html>" +
+//                "<body>" +
+//                "<p style='color:#0394fc'><font size='+15'>Name: " + name + "</font></p>" +
+//                "<ol>" +
+//                "<font size='+5'><li>" + firstLanguageSelect + "</li>" +
+//                "<li>" + secondLanguageSelect + "</li>" +
+//                "<li>" + thirdLanguageSelect + "</li></font>" +
+//                "</ol>" +
+//                "</body>" +
+//                "</html>";
 
-//                        "<html>" +
-//                        "<body>" +
-//                        "<p style='color:#0394fc'><font size='+15'>Name: " + name + "</font></p>" +
-//                        "<ol>" +
-//                        "<font size='+5'><li>" + firstLanguageSelect + "</li>" +
-//                        "<li>" + secondLanguageSelect + "</li>" +
-//                        "<li>" + thirdLanguageSelect + "</li></font>" +
-//                        "</ol>" +
-//                        "</body>" +
-//                        "</html>";
+                        "<html>" +
+                        "<body>" +
+                                "<h1>Name: " + name + "</h1>" +
+                                "<table> " +
+                                "<tr></tr>" +
+                                "<td>First Preference</td>" +
+                                "<td>" + firstLanguageSelect + "</td>" +
+                                "<tr></tr>"+
+                                "<td>Second Preference</td>" +
+                                "<td>" + secondLanguageSelect + "</td>" +
+                                "<tr></tr>"+
+                                "<td>Third Preference</td>" +
+                                "<td>" + thirdLanguageSelect + "</td>" +
+                                "<tr></tr>"+
+                                "</table>"+
+                        "</body>" +
+                        "</html>";
 
         return response;
     }
